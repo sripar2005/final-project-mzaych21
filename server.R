7b3a191ddd638818cd8873aa04e492ea3f96941a
rmdtran <rmtran@uw.edu>
2024-03-03 20:50:52 -0800

    ready

--- server.R
+++ server.R
@@ -1,6 +1,147 @@
 
+trav_and_res_df <- read.csv("trav_and_res.csv")  # set working directory to source file location.
+
 server <- function(input, output){
   
   # TODO Make outputs based on the UI inputs here
+  output$barplot <- renderPlotly({
+    selected_team_data <- reactive({
+      if (input$team_selector == "Seattle Kraken") {
+        filtered_data <- trav_and_res_df %>%
+          summarize(
+            Sea_wins = sum(grepl("Sea W", W.or.L)),
+            Sea_losses = -sum(grepl("Sea L", W.or.L))
+          )
+      } else if (input$team_selector == "Pittsburgh Penguins") {
+        filtered_data <- trav_and_res_df %>%
+          summarize(
+            Pitt_wins = sum(grepl("Pitt W", W.or.L)),
+            Pitt_losses = -sum(grepl("Pitt L", W.or.L))
+          )
+      } else {
+        filtered_data <- trav_and_res_df %>%
+          summarize(
+            Pitt_wins = sum(grepl("Pitt W", W.or.L)),
+            Pitt_losses = -sum(grepl("Pitt L", W.or.L)),
+            Sea_wins = sum(grepl("Sea W", W.or.L)),
+            Sea_losses = -sum(grepl("Sea L", W.or.L))
+          )
+      }
+      return(filtered_data)
+    })
+    
+    wins_losses <- reactive({
+      selected_team_data() %>%
+        pivot_longer(cols = everything(),
+                     names_to = "Category", values_to = "Games")
+    })
+    
+    barplot <- ggplot(wins_losses(), aes(x = Category, y = Games, fill = Category)) +
+      geom_bar(stat = "identity", width = 0.7) +
+      labs(title = "Kraken vs. Penguins", x = "Category", y = "Games") +
+      scale_fill_manual(values = c("Pitt_wins" = "#FFB81C", "Pitt_losses" = "#CC9612",
+                                   "Sea_wins" = "#75aadb", "Sea_losses" = "#4c7a9d")) +
+      theme(axis.line.x = element_line(color = "black", size = 1, linetype = "solid"))
+    
+    barplot <- barplot + coord_cartesian(ylim = c(-50, 50)) +
+      geom_hline(yintercept = 0, color = "black", size = 0.3)
+    #plotly::ggplotly(barplot)
+    return(ggplotly(barplot))
+  })
+  
+  output$mapplot <- renderPlotly({
+    
+    us_states <- map_data("state")
+    
+  
+    
+    states_with_arena <- subset(us_states, region %in% c("arizona", 
+                                                         "california", 
+                                                         "colorado", 
+                                                         "florida", 
+                                                         "illinois", 
+                                                         "massachusetts", 
+                                                         "missouri", 
+                                                         "nevada", 
+                                                         "new jersey", 
+                                                         "new york", 
+                                                         "north carolina", 
+                                                         "ohio", 
+                                                         "pennsylvania", 
+                                                         "tennessee", 
+                                                         "texas", 
+                                                         "washington", 
+                                                         "district of columbia"))
+    
+    is_sea <- subset(us_states, region %in% c("washington"))
+    
+    far_from_sea <- subset(us_states, region %in% c("texas", 
+                                                    "missouri", 
+                                                    "illinois"))
+    
+    close_to_sea <- subset(us_states, region %in% c("arizona", 
+                                                    "california", 
+                                                    "colorado", 
+                                                    "nevada"))
+    
+    very_far_from_sea <- subset(us_states, region %in% c("florida", 
+                                                         "massachusetts", 
+                                                         "new jersey", 
+                                                         "new york", 
+                                                         "north carolina", 
+                                                         "ohio", 
+                                                         "pennsylvania", 
+                                                         "tennessee", 
+                                                         "district of columbia"))
+    
+    is_pitt <- subset(us_states, region %in% c("pennsylvania"))
+    
+    far_from_pitt <- subset(us_states, region %in% c("texas", 
+                                                    "colorado", 
+                                                    "arizona"))
+    
+    close_to_pitt <- subset(us_states, region %in% c("new york", 
+                                                    "new jersey", 
+                                                    "district of columbia", 
+                                                    "ohio",
+                                                    "illinois",
+                                                    "missouri",
+                                                    "tennessee",
+                                                    "north carolina",
+                                                    "florida"))
+    
+    very_far_from_pitt <- subset(us_states, region %in% c("california", 
+                                                         "nevada",
+                                                         "washington"))
+    
+  
+    
+    if (input$team_selector == "Seattle Kraken") {
+      mapplot <- ggplot() +
+        geom_polygon(data = us_states, aes(x = long, y = lat, group = group), fill = "aliceblue", color = "black") +
+        geom_polygon(data = is_sea, aes(x = long, y = lat, group = group), fill = "darkseagreen2") +
+        geom_polygon(data = close_to_sea, aes(x = long, y = lat, group = group), fill = "yellow") +
+        geom_polygon(data = far_from_sea, aes(x = long, y = lat, group = group), fill = "orange") +
+        geom_polygon(data = very_far_from_sea, aes(x = long, y = lat, group = group), fill = "red") +
+        labs(title = "Distance From Seattle") 
+    } else if (input$team_selector == "Pittsburgh Penguins") {
+      mapplot <- ggplot() +
+        geom_polygon(data = us_states, aes(x = long, y = lat, group = group), fill = "aliceblue", color = "black") +
+        geom_polygon(data = is_pitt, aes(x = long, y = lat, group = group), fill = "darkseagreen2") +
+        geom_polygon(data = close_to_pitt, aes(x = long, y = lat, group = group), fill = "yellow") +
+        geom_polygon(data = far_from_pitt, aes(x = long, y = lat, group = group), fill = "orange") +
+        geom_polygon(data = very_far_from_pitt, aes(x = long, y = lat, group = group), fill = "red") +
+        labs(title = "Distance From Pittsburgh") 
+    } else {
+      mapplot <- ggplot() +
+        geom_polygon(data = us_states, aes(x = long, y = lat, group = group), fill = "aliceblue", color = "black") +
+        geom_polygon(data = states_with_arena, aes(x = long, y = lat, group = group), fill = "darkseagreen2") +
+        labs(title = "Neutral Plot of All States") 
+    }
+    
+   
+    return(mapplot)
+  })
+  
   
 }
--- ui.R
+++ ui.R
@@ -1,48 +1,98 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(plotly)
+library(maps)
+
+trav_and_res_df <- read.csv("trav_and_res.csv")
 
 
 ## OVERVIEW TAB INFO
 
-overview_tab <- tabPanel("Overview Tab Titleasasa",
-   h1("Some title"),
-   p("some explanation")
+overview_tab <- tabPanel("Overview Tab Title",
+                         h1("Some title"),
+                         p("some explanation")
 )
 
 ## VIZ 1 TAB INFO
 
 viz_1_sidebar <- sidebarPanel(
-  h2("Options for graph"),
-  #TODO: Put inputs for modifying graph here
+  h2("Choose Your Team!"),
+  selectInput(
+    inputId = "team_selector",
+    label = "Select a Team:",
+    choices = c("Pittsburgh Penguins", "Seattle Kraken", "Both"),
+    selected = "Both"
+  )
 )
 
 viz_1_main_panel <- mainPanel(
-  h2("Vizualization 1 Title"),
-  # plotlyOutput(outputId = "your_viz_1_output_id")
+  h2("US Map where states with at least one NHL Arena are colored in"),
+  plotlyOutput(outputId = "mapplot")
 )
 
 viz_1_tab <- tabPanel("Viz 1 tab title",
-  sidebarLayout(
-    viz_1_sidebar,
-    viz_1_main_panel
-  )
+                      sidebarLayout(
+                        viz_1_sidebar,
+                        viz_1_main_panel
+                      )
 )
 
 ## VIZ 2 TAB INFO
+## Basic bar graph of total wins for the two teams
+## Also, we could have 4 bars, Pitt W and Pitt L and Seattle W and Seattle L.
 
 viz_2_sidebar <- sidebarPanel(
-  h2("Options for graph"),
-  #TODO: Put inputs for modifying graph here
+  h2("Choose Your Team!"),
+  selectInput(
+    inputId = "team_selector",
+    label = "Select a Team:",
+    choices = c("Pittsburgh Penguins", "Seattle Kraken", "Both"),
+    selected = "Both"
+  ),
+  conditionalPanel(
+    condition = "input.team_selector == 'Pittsburgh Penguins'",
+    tags$div(
+      h4("Percentage:"),
+      tags$p("Pitt wins: 50%"),
+      tags$p("Pitt losses: 50%"),
+      tags$p("Out of total 82 games")
+    )
+  ),
+  conditionalPanel(
+    condition = "input.team_selector == 'Seattle Kraken'",
+    tags$div(
+      h4("Percentage:"),
+      tags$p("Sea wins: 43%"),
+      tags$p("Sea losses: 57%"),
+      tags$p("Out of total 82 games")
+    )
+  ),
+  conditionalPanel(
+    condition = "input.team_selector == 'Both'",
+    tags$div(
+      h4("Percentage:"),
+      tags$p("Sea wins: 43%"),
+      tags$p("Sea losses: 57%"),
+      tags$p("Pitt wins: 50%"),
+      tags$p("Pitt losses: 50%"),
+      tags$p("Out of total 82 games")
+    )
+  )
 )
 
+
 viz_2_main_panel <- mainPanel(
-  h2("Vizualization 2 Title"),
+  h2("Total Wins and Losses"),
   # plotlyOutput(outputId = "your_viz_1_output_id")
+  plotlyOutput(outputId = "barplot")
 )
 
 viz_2_tab <- tabPanel("Viz 2 tab title",
-  sidebarLayout(
-    viz_2_sidebar,
-    viz_2_main_panel
-  )
+                      sidebarLayout(
+                        viz_2_sidebar,
+                        viz_2_main_panel
+                      )
 )
 
 ## VIZ 3 TAB INFO
@@ -58,25 +108,25 @@ viz_3_main_panel <- mainPanel(
 )
 
 viz_3_tab <- tabPanel("Viz 3 tab title",
-  sidebarLayout(
-    viz_3_sidebar,
-    viz_3_main_panel
-  )
+                      sidebarLayout(
+                        viz_3_sidebar,
+                        viz_3_main_panel
+                      )
 )
 
 ## CONCLUSIONS TAB INFO
 
 conclusion_tab <- tabPanel("Conclusion Tab Title",
- h1("Some title"),
- p("some conclusions")
+                           h1("Some title"),
+                           p("some conclusions")
 )
 
 
 
 ui <- navbarPage("Example Project Title",
-  overview_tab,
-  viz_1_tab,
-  viz_2_tab,
-  viz_3_tab,
-  conclusion_tab
+                 overview_tab,
+                 viz_1_tab,
+                 viz_2_tab,
+                 viz_3_tab,
+                 conclusion_tab
 )
